59ff7161ca51e34152b942aaeac8507366d36a4e
kaneko-joei <kaneko.joei@gmail.com>
2021-05-06 17:17:29 +0900

    商品詳細機能　提出

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,19 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
-    @item = Item.new
+    if user_signed_in?
+       @item = Item.new
+    else
+       redirect_to user_session_path(@item.id)
+    end
   end
 
   def create
@@ -19,12 +25,27 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   
 
   private
 
   def item_params
     params.require(:item).permit(:image,:name, :price, :description, :status_id, :shipping_cost_id, :shipping_day_id, :category_id, :prefecture_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+
   end
 
 
