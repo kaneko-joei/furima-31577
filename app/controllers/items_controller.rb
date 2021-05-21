1be868622d5e1d1946ffd5a98393f762ef88ed0b
kaneko-joei <kaneko.joei@gmail.com>
2021-05-21 12:15:49 +0900

    商品削除機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item,only: [:show,:edit,:update,:destroy]
-  before_action :item_seller_confirmation,only: [:edit,:update]
+  before_action :item_seller_confirmation,only: [:edit,:update,:destroy]
 
   
 
