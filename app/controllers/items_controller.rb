3c211dedb23d542ece78c5bf6c9833601d886233
kaneko-joei <kaneko.joei@gmail.com>
2021-05-21 12:07:00 +0900

    商品削除機能　提出

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item,only: [:show,:edit,:update]
+  before_action :set_item,only: [:show,:edit,:update,:destroy]
   before_action :item_seller_confirmation,only: [:edit,:update]
 
   
@@ -40,10 +40,10 @@ class ItemsController < ApplicationController
     end
 
 
-  # def destroy
-  #   @item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   
 
   private
