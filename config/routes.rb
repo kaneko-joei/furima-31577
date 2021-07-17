f62ce59fcd277392391639affe13aacfd86a6bae
kaneko-joei <kaneko.joei@gmail.com>
2021-07-17 20:03:59 +0900

    商品購入機能　修正８

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  get 'cards/new'
   devise_for :users
 
-  
   root to: 'items#index'
   
-
-
   resources :items do
     resources :purchases,only: [:index, :create]
-    resources :cards, only: [:new, :create]
+    
     
   end
-
-  
-
-
 end
